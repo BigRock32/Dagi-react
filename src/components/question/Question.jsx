ec88f2c6f06517dfbae38ab893cf3112ee7bbb00
BigRock32 <vasya.samarin.00@mail.ru>
2022-12-30 17:48:57 +0800

    add scrollbar

--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -2,17 +2,19 @@ import st from './question.module.scss'
 
 import questArrow from './../../media/question-arrow.svg'
 
-const Question = ({question, index, toggleQuestions}) => {
+const Question = ({ question, index, toggleQuestions }) => {
    return (
-      <div className={`${st.question}` + `${(question.open ? ` ${st._open}` : '' )}`} onClick={() => toggleQuestions(index) } 
-      key={index}
+      <div className={`${st.question}` + `${(question.open ? ` ${st._open}` : '')}`} onClick={() => toggleQuestions(index)}
+         key={index}
       >
          <div className={st.question__head}>
             <div className={`${st.question__title} title-h2`}>{question.title}</div>
             <img src={questArrow} alt="" className={st.question__icon} />
          </div>
          <div className={st.question__body}>
-            <p className={st.question__text}>{question.text}</p>
+            <div className={st.question__textBox}>
+               <p className={st.question__text}>{question.text}</p>
+            </div>
          </div>
       </div>
    );
